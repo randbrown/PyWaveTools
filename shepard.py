3e3e40ea185c61bbeac472d1834a6a4f1c30c49c
Randy Brown <brown.randy@gmail.com>
2017-10-01 23:40:27 -0400

    Use numpy array instead of for loop.  This way we can change it to a glissando

--- shepard.py
+++ shepard.py
@@ -17,45 +17,35 @@ def normalpdf(x, u, o):
     return (1.0/o)*normalpdf_base((x-u)/o)
 
 def cos_dist(x, u, o=12.0):
-    return (1.0 + math.cos((2.0*math.pi * (x - u))/o))/2.0
+    return (1.0 + np.cos((2.0*math.pi * (x - u))/o))/2.0
 
-def shepard_util(times, freq, peak_freq, mult):
-    lf = math.log(freq*mult)
-    lp = math.log(peak_freq)
-    #amp = 1.0 - 2.0*(math.fabs(lp-lf)/lp)
-    #amp = 1.0 - (math.fabs(peak_freq-freq*mult)/peak_freq)
-
-    #amp = 1.0
-    print freq, peak_freq, lf, lp, mult
+def shepard_util(times, freq, mult):
     val = np.sin(freq*(2.0*math.pi) * mult * times)
     return val
 
 def shepardtone(times, freq, index):
     """generates a shepard tone using frequency multiples of the given frequency"""
     print freq
-    peak_freq = 262.0 * 1.5
-    #amp = ((-1.0/peak_freq) * np.abs(peak_freq-freq) + 1.0)
-    #print amp
     vals = []
 
     num_octaves = 10
 
     # lower octaves
     for osc in range(num_octaves, 0, -1):
-        vals += [shepard_util(times, freq, peak_freq, 2.0**(-osc))]
+        vals += [shepard_util(times, freq, 2.0**(-osc))]
 
     # actual primary freq
-    vals += [shepard_util(times, freq, peak_freq, 1)]
+    vals += [shepard_util(times, freq, 1)]
 
     # higher octaves
     for osc in range(1, num_octaves):
-        vals += [shepard_util(times, freq, peak_freq, 2.0**(osc))]
+        vals += [shepard_util(times, freq, 2.0**(osc))]
 
     #vals = [vals[i] * normalpdf(i, 0, len(vals)/2.0) for i in range(0, len(vals))]
     vals_combined = np.zeros(vals[0].shape)
     for i in range(0, len(vals)):
         #pdf = normalpdf(i, len(vals)*((index)/12.0), len(vals)/3.0)
-        pdf = cos_dist(i, len(vals)*(.48 + ((index)/12.0) * .04), len(vals))
+        pdf = cos_dist(i, len(vals)*(1.0 - (.4 + ((index)/12.0) * .2)), len(vals))
         print 'pdf = ', pdf
         vals_combined += vals[i] * pdf
 
@@ -64,16 +54,23 @@ def shepardtone(times, freq, index):
 def main():
     """main function"""
     # times is array of values at each time slot of the whole wav file
-    times = wavelib.createtimes(DURATION, SAMPLE_RATE)
+    times = wavelib.createtimes(12 * DURATION, SAMPLE_RATE)
 
     vals_list = np.arange(0, 0)
 
+    #start_freq = 263.0
+
+    start_freq = 440.0 * (2 ** ((times//1)/12.0))
+
+    vals_list = wavelib.normalize(shepardtone(times, start_freq, times))
+
+    """
     for m in range(0, 12):
-        vals = shepardtone(times, 2.0**(float(m)/12.0)*(263.0), m)
+        vals = shepardtone(times, 2.0**(float(m)/12.0)*start_freq, m)
         vals = wavelib.normalize(vals)
         wavelib.write_wave_file('output/shepard' + str(m) + '.wav', vals)
         vals_list = np.concatenate((vals_list, vals), axis=0) 
-
+    """
     # and lets run through it twice, so cat the list together with itself 
     vals_list = np.concatenate((vals_list, vals_list), axis=0) 
     wavelib.write_wave_file('output/shepard_steps2x.wav', vals_list)
